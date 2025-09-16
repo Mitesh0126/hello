0ee070536d33f5f89e0cb8675638ed3980e8c514
Mitesh0126 <miteshghanchi1oct05@gmail.com>
2025-09-16 16:16:12 +0530

    Update script.js

--- frontend/script.js
+++ frontend/script.js
@@ -122,8 +122,8 @@ async function apiCall(endpoint, options = {}) {
         headers['Authorization'] = `Bearer ${token}`;
     }
 
-    try {
-        const response = await fetch(`${API_BASE_URL}${endpoint}`, {
+     try {
+        const response = await fetch(`https://hello1-krcb.onrender.com${endpoint}`, {
             ...options,
             headers
         });
